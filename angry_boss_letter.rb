8c776deabb45968cc4720deaec567b0eedb5ee07
Harleigh Abel <harleighabel@gmail.com>
2017-02-17 00:52:54 -0700

    first commit

---
+++ angry_boss_letter.rb
@@ -0,0 +1,53 @@
+=begin
+Harleigh Abel
+Feb. 15, 2017
+Learn to Program by Chris Pine
+Chap6 pg 32
+angry_boss_letter
+=end
+
+puts "\n\n\nDear  Employee,\n\nI feel like there is more you want out of this position that you are\n"
+puts "not verbalizing. Now is your chance!\n\n"
+puts "signed,\n\n\nWorld\'s Greatest Boss\n\n\n\n\n"
+
+
+complaint_1 = "I have blisters and papercuts all over"
+complaint_2 = "my hands from filing all these sharp papers."
+complaint_3 = "And six dollars an hour?!?"
+complaint_4 = "that rabid racoon"
+final_reply = "you\'re fired "
+
+puts "\nDear Boss,\n"
+puts "\nYou are correct. I\'ve been holding back.\n"
+puts "But since you\'ve asked there are many changes I\'d like to see around here!\n"
+puts "The hours are long. 16 hour shifts are sooooooooooo long. They are cruel and\n"
+puts "inhumane. I sleep in my office because there is no time to go home, shower or\n" 
+puts "eat and I havent brushed my teeth in 3 days! My wife is going to divorce me\n" 
+puts "because I haven't been home this week." + complaint_1 + "\n" 
+puts complaint_2 + " " + complaint_3 + "\n"
+puts "Is that even legal?\n\n"
+puts "Plus I\'m really tired of having to fight off " + complaint_4 + " everytime\n" 
+puts  "I use the copy machine! You really outta get animal control out here,\n" 
+puts "just today I\'ve seen three more employees foaming at the mouth potentially\n"
+puts "infected.\n\n"
+puts "signed,\n\n\n"
+puts "Your Humble Potentially Infectious Employee\n\n\n"
+
+puts "\n\n\nDear Employee,\n\n"
+puts "You ungrateful little @*$%!\n"
+puts "I took you in off the street and gave you a job, for that you should be grateful!\n\n"
+puts "\"" + complaint_1.upcase + complaint_2.upcase + "\""
+puts "\nWhat a whinner! In my day we walked five miles to file each paper!\n"
+puts "Each piece of paper was made of raw pine bark, that would tear and our fingers\n"
+puts "till they bled and we didn\'t complain!\n" +  "\n\"" + complaint_3.upcase  + "\"\n\n"
+puts "You're only worth $2.50 an hour a most.  I could swim to Egypt and back faster than\n"
+puts "you file papers! And for your information " + complaint_4.upcase + " is my life long family\n"
+puts "pet and friend Charlie, and he has an allergy to fluorescent lights and takes\n"
+puts "refuge behind the copy machine!"
+puts "\nBut most importantly I have just one thing to say...\n\n" 
+puts final_reply.upcase + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n"
+puts "signed,\n\n"
+puts "No Longer Your Boss!\n\n\n"
+
+
+
